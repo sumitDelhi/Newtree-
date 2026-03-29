06486ecaca6a5f2e479b40e76670e62c81fa7681
sumit <sumitguptabio112@gmail.com>
2026-03-29 22:38:10 +0530

    comment

--- TREE-APP-main/vite.config.ts
+++ TREE-APP-main/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Newtree-/', // exactly
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
